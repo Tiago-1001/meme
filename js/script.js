567902f89037db9064aac427b3b2cff89e6dc185
Tiago F. <76789858+Tiago-1001@users.noreply.github.com>
2024-09-16 10:31:16 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -7,8 +7,11 @@ function moveButton(x) {
     x.style.top = `${number}px`;
 }
 
-function great() {
-    alert('Parabéns, nota dó :0')
+function great(x) {
+    number = Math.floor(Math.random()*600);
+
+    x.style.left = `${number}px`;
+    x.style.top = `${number}px`;
 }
 
 function yes() {
